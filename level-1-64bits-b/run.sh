2625d06ffee796bd89971fbb04e0a217fda7cfb8
Ashish Sonone <ashson07@gmail.com>
2014-02-02 00:36:26 +0530

    level-1b : created the folder level-1-64bits-b

---
+++ level-1-64bits-b/run.sh
@@ -0,0 +1,17 @@
+if [ "$1" = "c" ]
+then
+    make -f Makefile.cfg clean
+    make -f Makefile.cfglp clean
+elif [ "$1" = "1" ]
+then
+    make -f Makefile.cfg FILE=test.c
+elif [ "$1" = "2" ]
+then
+    make -f Makefile.cfglp
+    ./cfglp test_files/test.cs306.cfg -tokens
+elif [ "$1" = "r" ]
+then
+    ./cfglp test_files/test.cs306.cfg -tokens
+else
+    echo "wrong code"
+fi
