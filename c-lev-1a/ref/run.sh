e0bf9488a9973ff5fd8c1085e6273d8ee5d5e4ee
Ashish Sonone <ashson07@gmail.com>
2014-03-19 19:20:31 +0530

    c-lev 1a : updated run.sh clean 'c' option

--- c-lev-1a/ref/run.sh
+++ c-lev-1a/ref/run.sh
@@ -1,8 +1,14 @@
 if [ "$1" = "c" ]
 then
     make -f Makefile.cfg clean
+    rm test_files/*.toks
+    rm test_files/*.ast
+    rm test_files/*.eval
+    rm test_files/*.sym
+    rm test_files/*.prog
     rm test_files/*.ic
     rm test_files/*.spim
+
 elif [ "$1" = "2" ]
 then
     make -f Makefile.cfglp
