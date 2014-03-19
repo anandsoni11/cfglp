baa1a977067bcce78259c8853183bbe13a9feaea
Ashish Sonone <ashson07@gmail.com>
2014-03-19 18:59:37 +0530

    c-lev 1a : done checking ecfg files, updated script icode.py

--- c-lev-1a/ref/icode.py
+++ c-lev-1a/ref/icode.py
@@ -29,6 +29,7 @@ for f in files:
 print("Done generating cfg files\n");
 
 print(cfgFiles)
+print("#cfg Files ", len(cfgFiles))
 
 for f in cfgFiles:
     print("Testing file  " + f ); 
@@ -39,9 +40,30 @@ for f in cfgFiles:
 
     os.system("cp " + icodefile + "  icode2.txt");
 
-    command = "./cfglp64 -tokens -icode " + f
+    command = "./cfglp64 -icode " + f
     os.system(command)
 
     os.system("cp " + icodefile + "  icode1.txt");
 
+    os.system("diff -bB icode1.txt icode2.txt");
+
+
+#ecfg files
+print("-------------------\nCHECKING ecfg FILES\n");
+
+error_files=[]
+for f in files:
+    fileName,fileExt = os.path.splitext( path + f)
+    if( fileExt == '.ecfg'):
+        error_files.append(path + "/" +f)
+print(error_files)
+print("#ecfg Files ", len(error_files))
+
+
+for f in error_files:
+    print("Testing file  " + f ); 
+    command = "./r_cfglp64 -icode  " + f + " 2> icode2.txt " 
+    os.system(command) 
+    command = "./cfglp64 -icode " + f + " 2> icode1.txt " 
+    os.system(command)
     os.system("diff -bB icode1.txt icode2.txt");
